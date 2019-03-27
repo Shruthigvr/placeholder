13fb68d64974f8fee307967f629229a883af7e4a
shruthigvr <shruthigvr@gmail.com>
2019-03-27 14:44:54 +0530

    Placeholder first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Home from './Home.js';
+import Profile from './Profile.js';
+import ReactDOM from 'react-dom';
+import Add from './Add.js';
+import {BrowserRouter as Router, Link,NavLink,Redirect} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+              <div className="App">                
+                 
+                  <Route path = "/" exact component = {Home} />        
+                  
+              </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
